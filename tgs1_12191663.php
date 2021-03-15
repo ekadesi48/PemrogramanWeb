2ee13a01959a6be41f8806080678344c65467567
ekadesi48 <62577646+ekadesi48@users.noreply.github.com>
2021-03-16 05:02:57 +0700

    Add files via upload

---
+++ tgs1_12191663.php
@@ -0,0 +1,13 @@
+<?php
+ //12191663_ Eka Desi Ramadani
+ 
+$ikanku = array("Teri","Lele","Bawal","Tuna");
+	echo "Ikan $ikanku[0] sering dijumpai di nasi kucing angkringan dan saya konsumsi pas tanggal tua.Kalo tanggal muda sering kuliner $ikanku[1],$ikanku[2], dan $ikanku[3]";
+	echo "<br />";
+	echo "<br />";
+?>
+
+<?php
+$date = "2017-03-14";
+echo date ("d-m-Y", strtotime ($date));
+?>
